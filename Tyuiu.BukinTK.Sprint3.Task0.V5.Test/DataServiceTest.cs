ea4a1bcb30f60132dd0064686c4a350b1e67f206
forengeit345 <timophey2907@gmail.com>
2025-10-18 19:22:58 +0500

    Add project files.

---
+++ Tyuiu.BukinTK.Sprint3.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BukinTK.Sprint3.Task0.V5.Lib;
+
+namespace Tyuiu.BukinTK.Sprint3.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int endValue = 10;
+
+            double res = ds.GetSumSeries(startValue, endValue);
+
+            double wait = 81.083;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint3.Task0.V5/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BukinTK.Sprint3.Task0.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = 1;
+        int stopValue = 10;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}
