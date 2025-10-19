b931965dcdf5bf3da7057527fe8bdb00a358022a
forengeit345 <timophey2907@gmail.com>
2025-10-19 13:09:47 +0500

    Task2

---
+++ Tyuiu.BukinTK.Sprint3.Task2.V24.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BukinTK.Sprint3.Task2.V24.Lib
+{
+    public class DataService : ISprint3Task2V24
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double result = 1.0;
+            while (startValue <= stopValue)
+            {
+                result *= (Math.Pow(value, startValue) + 4) * Math.Cos(value);
+                startValue++;
+            }
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint3.Task2.V24/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BukinTK.Sprint3.Task2.V24.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double value = 0.25;
+        int startValue = 1;
+        int stopValue = 8;
+
+        Console.WriteLine("Переменная A = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}
