5086e8d50af71a71c9cf03291c70ac9582383241
forengeit345 <timophey2907@gmail.com>
2025-10-25 17:46:33 +0500

    Task3

---
+++ Tyuiu.BukinTK.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.BukinTK.Sprint3.Task3.V10.Lib;
+
+namespace Tyuiu.BukinTK.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+
+            string str = "gdfppf vfppt p";
+            char chr = 'p';
+
+            string wait = "gdff vft ";
+            string res = ds.DeleteCharInString(str, chr);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint3.Task3.V10/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BukinTK.Sprint3.Task3.V10.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string str = "gdfppf vfppt p";
+        char chr = 'p';
+
+        Console.WriteLine("Весь текст = " + str);
+        Console.WriteLine("Буква которую надо удалить = " + chr);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.DeleteCharInString(str,chr));
+
+        Console.ReadKey();
+    }
+}
