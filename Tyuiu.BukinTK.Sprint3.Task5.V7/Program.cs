1dcf6691043777ef8043fc4850ddb7fd14d24b54
forengeit345 <timophey2907@gmail.com>
2025-10-26 16:47:13 +0500

    Task5

---
+++ Tyuiu.BukinTK.Sprint3.Task5.V7/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BukinTK.Sprint3.Task5.V7.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 5;
+        int startValue1 = 1;
+        int startValue2 = 1;
+        int stopValue1 = 3;
+        int stopValue2 = 10;
+
+        Console.WriteLine("Переменная X = " + x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+        Console.ReadKey();
+    }
+}
